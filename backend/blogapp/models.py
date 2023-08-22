72eb3ac44ae65bd2aebeef5c95bf17bd6f744294
Lavender Mise <lavendermise4@gmail.com>
2023-08-22 11:47:23 +0300

    initial commit

--- backend/blogapp/models.py
+++ backend/blogapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+def upload_to(instance,filename):
+    return "blogs/{filename}".format(filename=filename)
+class Blog(models.Model):
+    blogTitle=models.CharField(max_length=150)
+    blogPost=models.TextField()
+    blogImage=models.ImageField(("image"), upload_to=upload_to, default="blogs/default.png")
+    blogAuthor=models.CharField(max_length=50)
+    blogDate=models.DateTimeField(default=timezone.now())
+
+    def __str__(self):
+        return self.blogTitle
--- backend/blogapp/views.py
+++ backend/blogapp/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from .serializer import BlogSerializer
+from .models import Blog
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
+
 
 # Create your views here.
+class BlogView(APIView):
+    def get(self,request,format=None):
+        blogs=Blog.objects.all()
+        serializer=BlogSerializer(blogs,many=True)
+        return Response(serializer.data)
+    def post(self,request,format=None):  
+        serializer=BlogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.error,status=status.HTTP_400_BAD_REQUEST)
+class SingleBlogView(APIView):
+    def get_single_blog(self,id):
+        try:
+            return Blog.objects.get(id=id)
+        except Blog.DoesNotExist:
+            raise Http404
+    def get(self,request,id):
+        blog=self.get_single_blog(id)
+        serializer=BlogSerializer(blog)
+        return Response(serializer.data)
+    def put(self,request,id):
+        blog=self.get_single_blog(id)
+        serializer=BlogSerializer(blog,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    def delete(self,request,id):
+        blog=self.get_single_blog(id)
+        blog.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
+
